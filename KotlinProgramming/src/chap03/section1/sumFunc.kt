bb4c0cbc7c6af60d94948f0f218f183f1fe5eea5
infomuscle <infomuscle10@gmail.com>
2020-07-20 22:11:27 +0900

    03-1 sumFunc

--- KotlinProgramming/src/chap03/section1/sumFunc.kt
+++ KotlinProgramming/src/chap03/section1/sumFunc.kt
@@ -4,3 +4,11 @@ fun sum(a: Int, b: Int): Int {
     var sum = a + b
     return sum
 }
+
+fun main() {
+    var result1 = sum(3, 2)
+    var result2 = sum(6, 7)
+
+    println(result1)
+    println(result2)
+}
